2641c5468cc310d078ff679d2832c1c8a74a9022
Omar Santos <santosomar@gmail.com>
2020-05-07 00:37:45 -0400

    updating website

--- assets/slidervideo/script.js
+++ assets/slidervideo/script.js
@@ -7,7 +7,7 @@
 
         var result = url.match(/(http:|https:|)\/\/(player.|www.)?(vimeo\.com|youtu(be\.com|\.be|be\.googleapis\.com))\/(video\/|embed\/|watch\?v=|v\/)?([A-Za-z0-9._%-]*)(&\S+)?/);
 
-        if (result && /youtu\.?be/g.test(result[3])) {
+        if (result && /youtube/g.test(result[3])) {
             return 'youtube';
         } else if (result && /vimeo/g.test(result[3])) {
             return 'vimeo';
