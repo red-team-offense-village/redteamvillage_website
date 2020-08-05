f85425fa578a3dbbf5c400b2d1150aa9c81b8ac2
Omar Santos <santosomar@gmail.com>
2020-08-05 18:29:47 -0400

    updating site

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -569,9 +569,13 @@
                 } else if ($('input[name=animation]').length) {
                     $('input[name=animation]').remove();
 
-                    var $animatedElements = $('p, h1, h2, h3, h4, h5, a, button, small, img, li, blockquote, .mbr-author-name, em, label, input, textarea, .input-group, .iconbox, .btn-social, .mbr-figure, .mbr-map, .mbr-testimonial .card-block, .mbr-price-value, .mbr-price-figure, .dataTable, .dataTables_info').not(function() {
-                    	return $(this).parents().is('a, p, .navbar, .mbr-arrow, footer, .iconbox, .mbr-slider, .mbr-gallery, .mbr-testimonial .card-block, #cookiesdirective, .mbr-wowslider, .accordion, .tab-content, .engine, #scrollToTop');
-                    }).addClass('hidden animated');
+                    var $animatedElements = $('p, h1, h2, h3, h4, h5, a, button, small, img, li, blockquote, .mbr-author-name, em, label, input, select, textarea, .input-group, .form-control, .iconbox, .btn-social, .mbr-figure, .mbr-map, .mbr-testimonial .card-block, .mbr-price-value, .mbr-price-figure, .dataTable, .dataTables_info')
+                        .not(function() {
+                            return $(this).parents().is('a, p, .navbar, .mbr-arrow, footer, .iconbox, .mbr-slider, .mbr-gallery, .mbr-testimonial .card-block, #cookiesdirective, .mbr-wowslider, .accordion, .tab-content, .engine, #scrollToTop');
+                        })
+                        .not(function(){
+                            return $(this).parents().is('form') && $(this).is('li')
+                        }).addClass('hidden animated');
 
                     function getElementOffset(element) {
                         var top = 0;
@@ -1082,6 +1086,7 @@
 
             $('section .form-with-styler').each(function () {
                 $(this).find('select:not("[multiple]")').each(function () {
+                    $(this).hide();
                     $(this).styler();
                 });
                 $(this).find('input[type=number]').each(function () {
@@ -1109,4 +1114,4 @@
             $(e.target).parents('.form-group').find('.value')[0].innerHTML = e.target.value;
         });
     }(jQuery));
-!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.ws">Mobirise Website Builder</a> v4.12.3';document.body.insertBefore(a,document.body.childNodes[0])}}();
+!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.ws">Mobirise Website Builder</a> v4.12.4';document.body.insertBefore(a,document.body.childNodes[0])}}();
